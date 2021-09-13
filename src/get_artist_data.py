21b5b75b92c4459bfefb76b4c66363f4c3026a10
Sobhan Badiozamany <sobhan.badiozamany@ica.se>
2021-09-13 21:56:16 +0200

    one API call per track, aggregated file per container

--- src/get_artist_data.py
+++ src/get_artist_data.py
@@ -9,7 +9,7 @@ from bigquery_utils import upload_file_to_bq
 def main():
     CLIENT_ID = os.getenv("CLIENT_ID")
     CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-    TRACK_ID = os.getenv("TRACK_ID")
+    TRACK_IDs = os.getenv("TRACK_ID")
 
     BASE_URL = 'https://api.spotify.com/v1/'
     AUTH_URL = 'https://accounts.spotify.com/api/token'
@@ -20,20 +20,15 @@ def main():
                                 AUTH_URL)
 
     ts = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    
-    print("{}: Processing".format(ts))
-    
-    tracks= spotify_client.get_tracks([TRACK_ID])
-
-    tracks_json_string = json.dumps(tracks)
-
-    timestamped_tracks = {"tracks":tracks_json_string,"extract_timestamp":ts}
-
     filename = 'tracks.json'
-
-    with open(filename, 'w') as file:
-        file.write(json.dumps(timestamped_tracks))
-
+    with open(filename, 'a') as file:
+        for track in TRACK_IDs.split(','):
+            track= spotify_client.get_tracks([track])
+            track_json_string = json.dumps(track)
+            timestamped_tracks = {"track":track_json_string,"extract_timestamp":ts}
+            file.write(json.dumps(timestamped_tracks))
+            file.write("\n")
+    
     upload_file_to_bq(
         filename=filename,
         project_id='capable-bivouac-325712',
